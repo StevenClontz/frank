457a723ba146f3cce8201e9afd05576f2ad7d370
Steven Clontz <steven.clontz@gmail.com>
2014-10-16 09:55:33 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frankApp
+ * @description
+ * # frankApp
+ *
+ * Main module of the application.
+ */
+angular.module('frankApp', [
+    'firebase',
+    'firebase.utils'
+  ]);
